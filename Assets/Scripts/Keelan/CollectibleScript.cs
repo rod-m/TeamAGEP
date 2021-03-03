6dde0e5866601ff2c2a0eed67416a2639a89eb7b
Keelan Smith <keelanjaysmith@live.co.uk>
2021-03-03 12:23:05 +0000

    Keelan's Collectible Stuff

--- Assets/Scripts/Keelan/CollectibleScript.cs
+++ Assets/Scripts/Keelan/CollectibleScript.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CollectibleScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private int score = 0;
 
-    // Update is called once per frame
-    void Update()
+    void Collected()
     {
-        
+        Destroy(gameObject);
+        score++;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Collected();
+        }
     }
 }
