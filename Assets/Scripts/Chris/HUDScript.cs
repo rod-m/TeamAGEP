d44a07500f0a0694ed5df27b47fed28ec1c64ceb
Chris-M-Bennett <cmbennett1@hotmail.com>
2021-03-10 10:30:50 +0000

    Timer Reformat

--- Assets/Scripts/Chris/HUDScript.cs
+++ Assets/Scripts/Chris/HUDScript.cs
@@ -53,13 +53,13 @@ namespace Chris{
         }
         private void LateUpdate(){
             if(timerMinutes > 0 || timerSeconds > 0){
-                if(timerSeconds > 9){timerText.text = $"{timerMinutes}:{Mathf.RoundToInt(timerSeconds)}";}
-                else {timerText.text = $"{timerMinutes}:0{Mathf.RoundToInt(timerSeconds)}";}
-                timerSeconds -= 1 * Time.deltaTime;
                 if(timerSeconds <= 0){
                     timerMinutes -= 1;
                     timerSeconds = 59;
                 }
+                timerSeconds -= 1 * Time.deltaTime;
+                if(timerSeconds > 9){timerText.text = $"{timerMinutes}:{Mathf.RoundToInt(timerSeconds)}";}
+                else {timerText.text = $"{timerMinutes}:0{Mathf.RoundToInt(timerSeconds)}";}
             }
         }
     }
