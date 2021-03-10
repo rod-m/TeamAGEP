8809d447f0a33c595afb858f015e3c0b37198540
Chris-M-Bennett <cmbennett1@hotmail.com>
2021-03-10 10:39:57 +0000

    powerup development

---
+++ Assets/Scripts/Keelan/PowerupScript.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupScript : MonoBehaviour
+{
+    [SerializeField]private DevPlayerTest player;
+
+    private float speedMultiplier = 2f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ApplyEffects();
+        }
+    }
+
+    void ApplyEffects()
+    {
+        if (this.CompareTag("SpeedUp"))
+        {
+            player.speed *= speedMultiplier;
+        }
+
+        GetComponent<Renderer>().enabled = false;
+
+        Invoke("ReverseEffects", 5f);
+    }
+
+    void ReverseEffects()
+    {
+        if (this.CompareTag("SpeedUp"))
+        {
+            player.speed /= speedMultiplier;
+        }
+        
+        Destroy(gameObject);
+    }
+    
+}
