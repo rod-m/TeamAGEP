b95b4521689da9456e595f9ea99d280fab8012b6
Chris-M-Bennett <cmbennett1@hotmail.com>
2021-03-10 12:29:37 +0000

    Initial Event Manager

---
+++ Assets/Scripts/Chris/EventManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Chris{
+    public sealed class EventManager : GenericSingleton<EventManager>
+    {
+        Dictionary<string, UnityEvent> _eventDictionary;
+        
+        public override void Awake(){
+            base.Awake();
+            if(Instance._eventDictionary == null){
+                Instance._eventDictionary = new Dictionary<string, UnityEvent>();
+            }
+        }
+        private void StartListening(string eventName, UnityAction listener){
+            UnityEvent thisEvent = null;
+            if (Instance._eventDictionary.TryGetValue(eventName, out thisEvent)){
+                //Event name is in dictionary
+                thisEvent.AddListener(listener);
+            }else{
+                thisEvent = new UnityEvent();
+                thisEvent.AddListener(listener);
+                Instance._eventDictionary.Add(eventName, thisEvent);
+            } 
+        }
+        private void StopListening(string eventName, UnityAction listener){
+            if (Instance._eventDictionary == null) return;
+            UnityEvent thisEvent = null;
+            if (Instance._eventDictionary.TryGetValue(eventName, out thisEvent)){
+                thisEvent.RemoveListener(listener);
+            }
+        }
+        private void TriggerEvent(string eventName){
+            UnityEvent thisEvent = null;
+            if (Instance._eventDictionary.TryGetValue(eventName, out thisEvent)){
+                thisEvent.Invoke();
+            } 
+        }
+    }
+}
