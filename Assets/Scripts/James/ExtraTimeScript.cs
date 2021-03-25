c542acbe3f8b50e91903968e60437ee1e047a4ee
Dragonofelder <71701731+Dragonofelder@users.noreply.github.com>
2021-03-25 15:46:00 +0000

    25/3/21

--- Assets/Scripts/James/ExtraTimeScript.cs
+++ Assets/Scripts/James/ExtraTimeScript.cs
@@ -3,27 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 /*
- * James please write up what you want this class to do
+ * Item that adds extra time on collected by player
  */
 public class ExtraTimeScript : MonoBehaviour
 {
-    public object Collider;
-    public object Player;
-    
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter(Collider collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnCollisionEnter(Collision Player)
-    {
-        
+        if (collision.gameObject.tag == "Player")
+        {
+            print("Extra Time Picked Up");
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/James/KeyScript.cs
+++ Assets/Scripts/James/KeyScript.cs
@@ -1,20 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 /*
- * James please write up what you want this class to do
+ * Opens up/removes a door somewhere in the level
  */
 public class KeyScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter(Collider collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.gameObject.tag == "Player")
+        {
+            print("Key Picked Up");
+            Destroy(gameObject);
+        }
     }
 }
